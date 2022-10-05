2d17170a10301ca632e29bd758a3c5f551e0ff4d
Arseny <Bambaleiio@yandex.ru>
2022-10-05 21:34:54 +0300

    Private lesson

---
+++ 4.py
@@ -0,0 +1,10 @@
+viewing_years = int(input("годы в течении которых посетитель будет постоянно смотреть на экспонаты"))
+print("Кол-во просмотренных экспонатов:", viewing_years * 365 * 8 * 60 // 5)
+
+exhibits = int(input("кол-во красивых экспонатов"))
+
+all_time = exhibits * 5
+
+
+# year = 365 * 8 = 2920h = 175200m
+# X years Y hours Z min
